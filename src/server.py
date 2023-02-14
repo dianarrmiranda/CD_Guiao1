664d4bf24cd4ed6913e7d1dd1ca763007796dd39
Diana Miranda <dianarrmiranda@ua.pt>
2023-02-14 14:55:06 +0000

    Introdução dos seletores

--- src/server.py
+++ src/server.py
@@ -1,12 +1,48 @@
 """CD Chat server program."""
 import logging
+#imports que acrescentei 
+import socket
+import selectors
 
 logging.basicConfig(filename="server.log", level=logging.DEBUG)
 
+sel = selectors.DefaultSelector()
 
 class Server:
     """Chat Server process."""
-
+    
     def loop(self):
         """Loop indefinetely."""
+
+    def accept(sock, mask):
+        conn, addr = sock.accept()  # Should be ready
+        print('accepted', conn, 'from', addr)
+        conn.setblocking(False)
+        sel.register(conn, selectors.EVENT_READ, read)
+
+    def read(conn, mask):
+        data = conn.recv(1000)  # Should be ready
+        if data:
+            print('echoing', repr(data), 'to', conn)
+            conn.send(data)  # Hope it won't block
+        else:
+            print('closing', conn)
+            sel.unregister(conn)
+            conn.close()
+
+    sock = socket.socket()
+    sock.bind(('localhost', 1234))
+    sock.listen(100)
+    sock.setblocking(False)
+    sel.register(sock, selectors.EVENT_READ, accept)
+
+    while True:
+        events = sel.select()
+        for key, mask in events:
+            callback = key.data
+            callback(key.fileobj, mask)
+
+
+
+
 
